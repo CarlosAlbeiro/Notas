5add133d2d2088e999f94c57e504b100fa32c88e
CarlosAlbeiro <carlosalbeirogaleano@hotmail.com>
2024-07-22 19:50:39 -0500

    Instalacion librerias adicionales

---
+++ front/src/components/modal.js
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios"; //Permite hacer peticiones
+import Swal from "sweetalert2"
+
+const Modal = ({ isOpen, onClose }) => {
+  const [addNota, setAddNota] = useState('');
+  const escribir_nota =(event)=>{
+    setAddNota(event.target.value)
+  }
+  const crear_Notas = async () => {
+    console.log("Nota ->"+addNota)
+    try {
+        //Try para encampsular los posibles errores
+        const response = await axios.post("http://localhost:3700/crearNota", {
+            nota:addNota
+          //Peticion hecha por medio de axios a la url del api
+          // Puedes agregar params y headers aquí si es necesario
+        });
+        console.log("si sirvio");
+        console.log(response);
+        setAddNota("");
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Nota guardada exitosamente.",
+          showConfirmButton: false,
+          timer: 2500
+        });
+        onClose();
+  
+      } catch (error) {
+        //Encapsulado del error
+        console.log("no sirvio");
+        console.error(error);
+        Swal.fire({
+          position: "center",
+          icon: "error",
+          title: "Lo siento no pudismos guardar tu nota.",
+          showConfirmButton: false,
+          timer: 2500
+        });
+        onClose();
+      }
+    //Funcion landa, anonima o flecha asincrona para hacer la peticiopn a la url del api en express
+    // e.preventDefault(); // Previene la recarga de la página
+  };
+
+  if (!isOpen) return null;
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div
+        className="fixed inset-0 bg-black opacity-50"
+        onClick={onClose}
+      ></div>
+      <div className="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all max-w-lg w-full p-6 z-10">
+        <div className="flex justify-between items-center pb-3">
+          <h3 className="text-lg font-semibold">Agregar Nota</h3>
+          
+          <button onClick={onClose} className="text-black">
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div className="mt-2">
+          <textarea
+            className="w-full p-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            rows="4"
+            value={addNota}
+            onChange={escribir_nota}
+            placeholder="Escribe tu nota aqui..."
+          ></textarea>
+        </div>
+        <div className="mt-4 flex justify-end">
+          <button
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full mr-2"
+            onClick={onClose}
+          >
+            Cerrar
+          </button>
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full" onClick={crear_Notas}>
+            Guardar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { Modal };
---
+++ front/src/components/notas.js
@@ -0,0 +1,190 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios"; //Permite hacer peticiones
+import { format } from "date-fns";
+import { Modal } from "./modal.js";
+
+const contentClass = "text-sm text-card-foreground overflow-auto";
+
+const MainCard = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  }
+  
+  const [notas_bd, setNotas_bd] = useState([]);
+
+  const peticion_notas = async () => {
+    let notas = [];
+    //Funcion landa, anonima o flecha asincrona para hacer la peticiopn a la url del api en express
+    // e.preventDefault(); // Previene la recarga de la página
+
+    try {
+      //Try para encampsular los posibles errores
+      const response = await axios.get("http://localhost:3700/listarNota", {
+        //Peticion hecha por medio de axios a la url del api
+        // Puedes agregar params y headers aquí si es necesario
+      });
+      console.log("si sirvio");
+      console.log(response);
+
+      for (let i = 0; i < response.data.length; i++) {
+        let fecha = format(
+          new Date(response.data[i].fecha_creacion),
+          "dd/MM/yyyy"
+        );
+        let hora = format(
+          new Date(response.data[i].fecha_creacion),
+          "hh:mm:ss a"
+        );
+        let contenido = [response.data[i].nota, fecha, hora];
+        notas.push(contenido);
+      }
+    } catch (error) {
+      //Encapsulado del error
+      console.log("no sirvio");
+      console.error(error);
+      let contenido = [null, null, null];
+      notas.push(contenido);
+    }
+
+    setNotas_bd(notas);
+    console.log(notas);
+  };
+
+  const actualizar_Nota = async () => {
+    let notas = [];
+    //Funcion landa, anonima o flecha asincrona para hacer la peticiopn a la url del api en express
+    // e.preventDefault(); // Previene la recarga de la página
+
+    try {
+      //Try para encampsular los posibles errores
+      const response = await axios.get("http://localhost:3700/listarNota", {
+        //Peticion hecha por medio de axios a la url del api
+        // Puedes agregar params y headers aquí si es necesario
+      });
+      console.log("si sirvio");
+      console.log(response);
+
+      for (let i = 0; i < response.data.length; i++) {
+        let fecha = format(
+          new Date(response.data[i].fecha_creacion),
+          "dd/MM/yyyy"
+        );
+        let hora = format(
+          new Date(response.data[i].fecha_creacion),
+          "hh:mm:ss a"
+        );
+        let contenido = [response.data[i].nota, fecha, hora];
+        notas.push(contenido);
+      }
+    } catch (error) {
+      //Encapsulado del error
+      console.log("no sirvio");
+      console.error(error);
+      let contenido = [null, null, null];
+      notas.push(contenido);
+    }
+
+    setNotas_bd(notas);
+    console.log(notas);
+  };
+
+  const eliminar_Nota = async () => {
+    let notas = [];
+    //Funcion landa, anonima o flecha asincrona para hacer la peticiopn a la url del api en express
+    // e.preventDefault(); // Previene la recarga de la página
+
+    try {
+      //Try para encampsular los posibles errores
+      const response = await axios.get("http://localhost:3700/listarNota", {
+        //Peticion hecha por medio de axios a la url del api
+        // Puedes agregar params y headers aquí si es necesario
+      });
+      console.log("si sirvio");
+      console.log(response);
+
+      for (let i = 0; i < response.data.length; i++) {
+        let fecha = format(
+          new Date(response.data[i].fecha_creacion),
+          "dd/MM/yyyy"
+        );
+        let hora = format(
+          new Date(response.data[i].fecha_creacion),
+          "hh:mm:ss a"
+        );
+        let contenido = [response.data[i].nota, fecha, hora];
+        notas.push(contenido);
+      }
+    } catch (error) {
+      //Encapsulado del error
+      console.log("no sirvio");
+      console.error(error);
+      let contenido = [null, null, null];
+      notas.push(contenido);
+    }
+
+    setNotas_bd(notas);
+    console.log(notas);
+  };
+ 
+
+  useEffect(() => {
+    peticion_notas(); // Llama la función al montar el componente
+
+    const intervalId = setInterval(peticion_notas, 10000); // Llama cada 30 segundos
+
+    // Limpieza del intervalo al desmontar el componente
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-background">
+      <div className="grid w-full bg-card shadow-lg rounded-lg overflow-hidden  justify-items-center">
+        <div className="p-4 grid grid-cols-1 sm:grind-colds-2 fixed bg-white">
+          <h2 className="text-2xl font-bold text-primary mb-4">
+            Notas rapidas carlos
+          </h2>
+          <button className="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 w-80 p-1 text-white font-bold rounded-full inline-flex items-center justify-center"
+           onClick={openModal}>
+            Agregar nota
+          </button>
+          <Modal isOpen={isModalOpen} onClose={closeModal} />
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 scroll-auto mt-20">
+          {notas_bd.map((item) =>
+            item[0] == null ? (
+              <h1 class="rounded overflow-hidden shadow-lg m-3 p-1 center">Sin notas</h1>
+            ) : (
+              <Card
+                title="Card"
+                contenido={item[0]}
+                fecha={item[1]}
+                hora={item[2]}
+              />
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Card = ({ contenido, fecha, hora }) => {
+  return (
+    <div class="rounded overflow-auto shadow-lg mt-5 p-1 h-20">
+      <small class=" inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">
+        {fecha}
+        <br></br>
+        {hora}
+      </small>
+      <p className={contentClass}>{contenido}</p>
+    </div>
+  );
+};
+
+export { MainCard };
